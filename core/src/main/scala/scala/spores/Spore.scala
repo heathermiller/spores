5a200ee48869940e5272a3c68f960df3405c782f
Philipp Haller <hallerp@gmail.com>
2015-02-21 15:54:44 +0100

    Make underlying function accessible
    This is a temporary work-around for a current limitation in
    spores with more than one parameter. The macro implementation
    for these kinds of spores should be revisited.

--- core/src/main/scala/scala/spores/Spore.scala
+++ core/src/main/scala/scala/spores/Spore.scala
@@ -46,19 +46,19 @@ trait Spore2[-T1, -T2, +R] extends Function2[T1, T2, R]
 
 trait Spore3[-T1, -T2, -T3, +R] extends Function3[T1, T2, T3, R]
 
-class NullarySporeImpl[+R](f: () => R) extends NullarySpore[R] {
+class NullarySporeImpl[+R](val f: () => R) extends NullarySpore[R] {
   def apply(): R = f()
 }
 
-class SporeImpl[-T, +R](f: T => R) extends Spore[T, R] {
+class SporeImpl[-T, +R](val f: T => R) extends Spore[T, R] {
   def apply(x: T): R = f(x)
   def className = "SporeImpl"
 }
 
-class Spore2Impl[-T1, -T2, +R](f: (T1, T2) => R) extends Spore2[T1, T2, R] {
+class Spore2Impl[-T1, -T2, +R](val f: (T1, T2) => R) extends Spore2[T1, T2, R] {
   def apply(x1: T1, x2: T2): R = f(x1, x2)
 }
 
-class Spore3Impl[-T1, -T2, -T3, +R](f: (T1, T2, T3) => R) extends Spore3[T1, T2, T3, R] {
+class Spore3Impl[-T1, -T2, -T3, +R](val f: (T1, T2, T3) => R) extends Spore3[T1, T2, T3, R] {
   def apply(x1: T1, x2: T2, x3: T3): R = f(x1, x2, x3)
 }
