34e1f8009b841455cb45245e9714524836324195
Philipp Haller <hallerp@gmail.com>
2015-06-02 17:12:01 +0200

    0.2.0-SNAPSHOT

--- project/SporesBuild.scala
+++ project/SporesBuild.scala
@@ -9,7 +9,7 @@ object SporesBuild extends Build {
     organization := "org.scala-lang.modules",
     organizationName := "LAMP/EPFL",
     organizationHomepage := Some(new URL("http://lamp.epfl.ch")),
-    version      := "0.1.3",
+    version      := "0.2.0-SNAPSHOT",
     scalaVersion := "2.11.6"
   )
 
