5d46999ea6bb722bdb78ec4733bebf2cf0e6e84f
Alex Loiko <alex@loiko.se>
2016-02-12 13:24:20 +0100

    Added implicit conversion to Spore[...] {type Excluded = ...} to spores.
    * Tests in ExcludedBasic.scala and BasicNeg.scala show how the functionality is to be used.
    * All of NullarySpore, Spore, Spore2, Spore3 can be implicitly converted to `{type Excluded = ...}`.
    * This required the following changes to spores:
       1. Added "self =>" references to the generated classes in MacroImpl.{check, check2, checkNullary}.
       2. Changed the substitution of captured values to "captured._n" into "self.captured._n".
          We believe that test "ExcludedBasic.selfCaptureTypesTest show that this change is necessary.
       3. Changed the classes generated by MacroImpl.{check, check2, checkNullary} from "final" non derivable.
          This is needed for the Excluded functionality to work (see comments and code in ExcludedBasic.scala)

--- core/src/test/scala/scala/spores/neg/basic/BasicNeg.scala
+++ core/src/test/scala/scala/spores/neg/basic/BasicNeg.scala
@@ -185,3 +185,40 @@ class StablePathNegSpec {
     }
   }
 }
+
+@RunWith(classOf[JUnit4])
+class ExcludedSpec {
+
+  @Test
+  def nullaryWrong(): Unit = {
+    expectError("Expression has type 'String', but type 'String' is Excluded") {
+      """
+        import scala.spores._
+        import scala.spores.SporeConv._
+        val s: NullarySpore[Unit] {type Excluded = String} = spore {
+          val s = "hej"
+          delayed {
+            ()
+          }
+        }
+      """
+    }
+  }
+
+  @Test
+  def nullaryWrongMultiple(): Unit = {
+    expectError("Expression has type 'String', but type 'String' is Excluded") {
+      """
+        import scala.spores._
+        import scala.spores.SporeConv._
+        val s: NullarySpore[Unit] {type Excluded = (String, Int)} = spore {
+          val s = "hej"
+          delayed {
+            ()
+          }
+        }
+      """
+    }
+  }
+
+}
