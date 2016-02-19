8ec7159340c31c314477f5220a9d3a068c62c9b4
Philipp Haller <hallerp@gmail.com>
2016-02-19 12:05:54 +0100

    0.2.0

--- project/SporesBuild.scala
+++ project/SporesBuild.scala
@@ -9,7 +9,7 @@ object SporesBuild extends Build {
     organization := "org.scala-lang.modules",
     organizationName := "LAMP/EPFL",
     organizationHomepage := Some(new URL("http://lamp.epfl.ch")),
-    version      := "0.2.0-SNAPSHOT",
+    version      := "0.2.0",
     scalaVersion := "2.11.6"
   )
 
